62ca045805ebfdf4c37622ef618b2ea43959f654
Himanshu Jawla <himanshujawla230@gmail.com>
2025-09-29 23:43:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const planets = document.querySelectorAll(".planet");
 const infoPanel = document.getElementById("infoPanel");
 const planetName = document.getElementById("planetName");
 const planetFact = document.getElementById("planetFact");
-const closeBtn = document.getElementById("closeBtn");
 
+// Show info on hover
 planets.forEach(planet => {
-  planet.addEventListener("click", () => {
+  planet.addEventListener("mouseenter", () => {
     planetName.textContent = planet.dataset.name;
     planetFact.textContent = planet.dataset.fact;
     infoPanel.classList.add("show");
   });
+
+  planet.addEventListener("mouseleave", () => {
+    infoPanel.classList.remove("show");
+  });
 });
 
-closeBtn.addEventListener("click", () => {
-  infoPanel.classList.remove("show");
-});
